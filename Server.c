26d3ea0938f0ebdb8f0efdcb480b093b39cc136c
root <root@LAPTOP-9R6N6GJV.localdomain>
2020-07-15 22:51:35 +0200

    cleaned

--- Server.c
+++ Server.c
@@ -22,60 +22,8 @@ int gcm_decrypt(unsigned char *ciphertext, int ciphertext_len,
                 unsigned char *key,
                 unsigned char *iv, int iv_len,
                 unsigned char *plaintext);
-
-int receiveFromClient(char *port, unsigned char *ciphertext){
-    int sockfd, connfd, len; 
-    struct sockaddr_in servaddr, cli; 
-
-    // socket create and verification 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
-    if (sockfd == -1) { 
-        printf("socket creation failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully created..\n"); 
-    bzero(&servaddr, sizeof(servaddr)); 
-
-    // assign IP, port 
-    servaddr.sin_family = AF_INET; 
-    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
-    servaddr.sin_port = htons(port); 
-
-    // Binding newly created socket to given IP and verification 
-    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
-        printf("socket bind failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Socket successfully binded..\n"); 
-
-    // Now server is ready to listen and verification 
-    if ((listen(sockfd, 5)) != 0) { 
-        printf("Listen failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("Server listening..\n"); 
-    len = sizeof(cli); 
-
-    // Accept the data packet from client and verification 
-    connfd = accept(sockfd, (SA*)&cli, &len); 
-    if (connfd < 0) { 
-        printf("server acccept failed...\n"); 
-        exit(0); 
-    } 
-    else
-        printf("server acccept the client...\n"); 
-
-    // read the message from client and copy it in ciphertext 
-    read(sockfd, ciphertext, sizeof(ciphertext));  
-
-    // After chatting close the socket 
-    close(sockfd); 
-
-    return strlen((char*) ciphertext);
-}
+int receiveFromClient(char *port, 
+                      unsigned char *ciphertext);
 
 int main(int argc, char *argv[]){
    
@@ -211,4 +159,59 @@ int gcm_decrypt(unsigned char *ciphertext, int ciphertext_len,
         /* Verify failed */
         return -1;
     }
+}
+
+
+int receiveFromClient(char *port, unsigned char *ciphertext){
+    int sockfd, connfd, len; 
+    struct sockaddr_in servaddr, cli; 
+
+    // socket create and verification 
+    sockfd = socket(AF_INET, SOCK_STREAM, 0); 
+    if (sockfd == -1) { 
+        printf("socket creation failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully created..\n"); 
+    bzero(&servaddr, sizeof(servaddr)); 
+
+    // assign IP, port 
+    servaddr.sin_family = AF_INET; 
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY); 
+    servaddr.sin_port = htons(port); 
+
+    // Binding newly created socket to given IP and verification 
+    if ((bind(sockfd, (SA*)&servaddr, sizeof(servaddr))) != 0) { 
+        printf("socket bind failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Socket successfully binded..\n"); 
+
+    // Now server is ready to listen and verification 
+    if ((listen(sockfd, 5)) != 0) { 
+        printf("Listen failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("Server listening..\n"); 
+    len = sizeof(cli); 
+
+    // Accept the data packet from client and verification 
+    connfd = accept(sockfd, (SA*)&cli, &len); 
+    if (connfd < 0) { 
+        printf("server acccept failed...\n"); 
+        exit(0); 
+    } 
+    else
+        printf("server acccept the client...\n"); 
+
+    // read the message from client and copy it in ciphertext 
+    read(sockfd, ciphertext, sizeof(ciphertext));  
+
+    // After chatting close the socket 
+    close(sockfd); 
+
+    return strlen((char*) ciphertext);
 }
